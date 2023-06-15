f756df4d953727b4805db1eb905c8c28f1b3f304
darkshadow2074 <akashpre196@gmail.com>
2023-06-15 23:46:31 +0530

    Updated package json file with new dependecies and added code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import MyPost from "./Components/MyPost.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MyPost />
+    </div>
+  );
 }
 
 export default App;
